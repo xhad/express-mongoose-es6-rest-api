75abd0c3a00640bea93277210620fe61b5d03350
Kunal Kapadia <kunalkapadia12@gmail.com>
2017-02-09 01:01:40 +0530

    Add Dockerfile and docker-compose for development

--- config/env/development.js
+++ config/env/development.js
@@ -1,8 +1,10 @@
+const mongoHost = process.env.MONGO_HOST || 'localhost';
+
 const devConfig = {
   env: 'development',
   MONGOOSE_DEBUG: true,
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-  db: 'mongodb://localhost/express-mongoose-es6-rest-api-development',
+  db: `mongodb://${mongoHost}/express-mongoose-es6-rest-api-development`,
   port: 4040
 };
 
--- config/env/test.js
+++ config/env/test.js
@@ -1,7 +1,9 @@
+const mongoHost = process.env.MONGO_HOST || 'localhost';
+
 const testConfig = {
   env: 'test',
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-  db: 'mongodb://localhost/express-mongoose-es6-rest-api-test',
+  db: `mongodb://${mongoHost}/express-mongoose-es6-rest-api-test`,
   port: 4040
 };
 
